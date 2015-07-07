719cbbc56a47ba2e4a3086878d50cc7fcc54ffcf
jgartee <garteejohn@johndeere.com>
2015-07-07 16:46:03 -0400

    US119091: Added target directory tests and code

--- spec/upload_file_tracking_spec.rb
+++ spec/upload_file_tracking_spec.rb
@@ -10,4 +10,8 @@ describe "#Manage file i/o" do
   it "creates an object of the correct type" do
     expect(@tracker).to be_a UploadFileTracker
   end
+
+  it "returns the CI directory when /Network/NAS/nas_root is not available" do
+    expect(@tracker.targetDirectory).to eq(File.expand_path(File.dirname(__FILE__) + '/../ci'))
+  end
 end
--- upload_file_tracker.rb
+++ upload_file_tracker.rb
@@ -1,3 +1,5 @@
 class UploadFileTracker
-
+  def targetDirectory
+    File.expand_path(File.dirname(__FILE__) + '/ci') 
+  end
 end
